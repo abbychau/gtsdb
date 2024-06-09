01c48bb4ef2f467035f6c11bd5874a1b790d7f32
Abby <i@abby.md>
2024-06-10 07:41:31 +0900

    add tests

--- main_test.go
+++ main_test.go
@@ -78,6 +78,55 @@ func TestMainRead(t *testing.T) {
 	}
 }
 
+func TestDownsampleDataPoints(t *testing.T) {
+	dataPoints := []DataPoint{
+		{Timestamp: 1, Value: 1},
+		{Timestamp: 2, Value: 2},
+		{Timestamp: 3, Value: 3},
+		{Timestamp: 4, Value: 4},
+		{Timestamp: 5, Value: 5},
+		{Timestamp: 6, Value: 6},
+		{Timestamp: 7, Value: 7},
+		{Timestamp: 8, Value: 8},
+		{Timestamp: 9, Value: 9},
+		{Timestamp: 10, Value: 10},
+	}
+
+	downsampledDataPoints := downsampleDataPoints(dataPoints, 5)
+	if len(downsampledDataPoints) != 2 {
+		t.Error("Unexpected number of downsampled data points:", len(downsampledDataPoints))
+	}
+
+	if downsampledDataPoints[0].Timestamp != 1 || downsampledDataPoints[0].Value != 3 {
+		t.Error("Unexpected downsampled data point:", downsampledDataPoints[0])
+	}
+
+	if downsampledDataPoints[1].Timestamp != 6 || downsampledDataPoints[1].Value != 8 {
+		t.Error("Unexpected downsampled data point:", downsampledDataPoints[1])
+	}
+}
+
+// formatDataPoints
+func TestFormatDataPoints(t *testing.T) {
+	dataPoints := []DataPoint{
+		{ID: "test", Timestamp: 1, Value: 1},
+		{ID: "test", Timestamp: 2, Value: 2},
+		{ID: "test", Timestamp: 3, Value: 3},
+		{ID: "test", Timestamp: 4, Value: 4},
+		{ID: "test", Timestamp: 5, Value: 5},
+		{ID: "test", Timestamp: 6, Value: 6},
+		{ID: "test", Timestamp: 7, Value: 7},
+		{ID: "test", Timestamp: 8, Value: 8},
+		{ID: "test", Timestamp: 9, Value: 9},
+		{ID: "test", Timestamp: 10, Value: 10},
+	}
+
+	formattedDataPoints := formatDataPoints(dataPoints)
+	if formattedDataPoints != "test,1,1.00|test,2,2.00|test,3,3.00|test,4,4.00|test,5,5.00|test,6,6.00|test,7,7.00|test,8,8.00|test,9,9.00|test,10,10.00\n" {
+		t.Error("Unexpected formatted data points:", formattedDataPoints)
+	}
+}
+
 // benchmark
 // go test -run=nonthingplease -bench BenchmarkMain -benchtime 10s
 func BenchmarkMain(b *testing.B) {
