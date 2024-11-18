378be44d0010227eea884db60b3b174bee5e596a
Abby <i@abby.md>
2024-11-18 19:38:30 +0900

    feat: update buffer management by separating state variables and enhancing .gitignore

---
+++ buffer/states.go
@@ -0,0 +1,20 @@
+package buffer
+
+import (
+	"gtsdb/concurrent"
+	"gtsdb/models"
+	"gtsdb/synchronous"
+	"os"
+)
+
+const indexInterval = 5000
+
+const maxUnflushedDataPoints = 0
+
+var dataFileHandles = concurrent.NewHashMap[string, *os.File]()
+var indexFileHandles = concurrent.NewHashMap[string, *os.File]()
+var metaFileHandles = concurrent.NewHashMap[string, *os.File]()
+var idToRingBufferMap = concurrent.NewHashMap[string, *synchronous.RingBuffer[models.DataPoint]]()
+
+var lastValue = make(map[string]float64)
+var lastTimestamp = make(map[string]int64)
--- buffer/storage.go
+++ buffer/storage.go
@@ -10,18 +10,6 @@ import (
 	"os"
 )
 
-const indexInterval = 5000
-
-const maxUnflushedDataPoints = 0
-
-var dataFileHandles = concurrent.NewHashMap[string, *os.File]()
-var indexFileHandles = concurrent.NewHashMap[string, *os.File]()
-var metaFileHandles = concurrent.NewHashMap[string, *os.File]()
-var idToRingBufferMap = concurrent.NewHashMap[string, *synchronous.RingBuffer[models.DataPoint]]()
-
-var lastValue = make(map[string]float64)
-var lastTimestamp = make(map[string]int64)
-
 func StoreDataPointBuffer(dataPoint models.DataPoint) {
 	if maxUnflushedDataPoints == 0 {
 		storeDataPoints(dataPoint.ID, []models.DataPoint{dataPoint})
